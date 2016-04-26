f3dfecf4dfc57efae772f24ac00207dec34c83b0
Satya Naik <satya.pra.naik@hpe.com>
2016-04-26 10:46:41 -0700

    fix: all: Write error when disk is full
    Change-Id: I78a00236aa38a03db168f6a4850396c4257a0a97
    Signed-off-by: Satya Naik <satya.pra.naik@hpe.com>

--- include/diag_dump_vty.h
+++ include/diag_dump_vty.h
@@ -53,12 +53,13 @@ typedef struct
 #define     TRUE              1
 #endif
 
+#define DIAG_FILE_NAME_REGEX       "^([A-Za-z0-9_.-]){1,50}$"
 #define DIAG_DUMP_DIR              "/tmp/ops-diag"
 #define FILE_PATH_LEN_MAX          256
 #define MAX_TIME_STR_LEN           256
 #define MAX_CLI_STR_LEN            256
-#define USER_FILE_LEN_MAX          50
 #define DIAG_CMD_LEN_MAX           50
+#define MAX_STR_BUFF_LEN           512
 
 #define MAX_PID                    65536
 #define MIN_PID                    1
